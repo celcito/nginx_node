f16eba2efe4acb30178285d1dc90a309f15ae629
celcito <celso832@gmail.com>
2024-04-18 16:58:15 -0300

    feat:add mysql

--- app/index.js
+++ app/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database:'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //const sql = `INSERT INTO people(name) values('Celso')`
+  const sql = `select name from people `
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Erro ao executar a consulta');
+    } else {
+      const namesHtml = results.map(result => `<p>${result.name}</p>`).join('');
+      const response = `<h1>Full Cycle Rocks!</h1><p>Resultados da consulta:</p>${namesHtml}`;
+      res.send(response);
+    }
+    connection.end();
+ });
 })
 app.listen(5000, () => console.log('Server is up and running'));
