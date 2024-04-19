bd46481749b26d05833d621c1001bca50a48b3d7
celcito <celso832@gmail.com>
2024-04-19 11:16:33 -0300

    fix: create poll to control conections

--- app/index.js
+++ app/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
 
-const config = {
+const mysql = require('mysql')
+const pool = mysql.createPool({
   host: 'db',
   user: 'root',
   password: 'root',
-  database:'nodedb'
-};
+  database:'nodedb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
 app.get('/', (req, res) => {
   //const sql = `INSERT INTO people(name) values('Celso')`
   const sql = `select name from people `
-  connection.query(sql, (error, results) => {
+  pool.query(sql, (error, results) => {
     if (error) {
       console.error(error);
       res.status(500).send('Erro ao executar a consulta');
@@ -22,8 +24,8 @@ app.get('/', (req, res) => {
       const namesHtml = results.map(result => `<p>${result.name}</p>`).join('');
       const response = `<h1>Full Cycle Rocks!</h1><p>Resultados da consulta:</p>${namesHtml}`;
       res.send(response);
+    
     }
-    connection.end();
  });
 })
 app.listen(5000, () => console.log('Server is up and running'));
