ccdf8159e669991ffcc82ee14c5b8799a1a5d0ab
celcito <celso832@gmail.com>
2024-04-19 17:46:56 -0300

    feat:insert data

--- app/index.js
+++ app/index.js
@@ -2,15 +2,55 @@ const express = require('express');
 const app = express();
 
 const mysql = require('mysql')
-const pool = mysql.createPool({
+const config = {
   host: 'db',
   user: 'root',
   password: 'root',
   database:'nodedb',
+ };
+const pool = mysql.createPool({
+  ...config,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+
+
+
+
+
+const connection = mysql.createConnection(config)
+
+
+
+
+
+
+app.get('/insert', (req, res) => {
+  
+  const sql = `create table if not exists people  (id int not null auto_increment,name varchar(255),primary key(id))`
+  connection.query(sql)
+
+  const names = ['Wesley','Celso','fullcicle']
+  names.forEach(el=>{
+    let sqlInsert = 'INSERT INTO people(name) VALUES(\'' + el + '\')';
+    connection.query(sqlInsert, (error, results) => {
+      if (error) {
+        console.error('Erro ao inserir nome:', error);
+        
+      } else {
+        console.log('Nome inserido com sucesso:', results);
+      }
+   });
+  })
+  connection.query(sql)
+  res.send('<h1>Full Cycle</h1>')
+
+})
+
+
+
 
 
 app.get('/', (req, res) => {
